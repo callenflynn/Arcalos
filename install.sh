643f432eb2b2ba5d4bd5a422357b95c84b7d8271
Callen Flynn <not.skittle.or.beluga@gmail.com>
2026-05-21 14:42:59 -0400

    fix permission issue

--- install.sh
+++ install.sh
@@ -10,16 +10,33 @@ export ARCALOS_INSTALL_LOG_FILE="/var/log/arcalos-install.log"
 export PATH="$ARCALOS_PATH/bin:$PATH"
 
 # Ensure arcalos-* command shims exist during install (repo ships omarchy-* binaries).
+ensure_arcalos_shim() {
+  local command_path="$1"
+  local command_name="${command_path##*/}"
+  local arcalos_name="arcalos-${command_name#omarchy-}"
+  local arcalos_path="$ARCALOS_PATH/bin/$arcalos_name"
+
+  if [[ -e $arcalos_path ]]; then
+    if [[ ! -x $arcalos_path ]]; then
+      chmod +x "$arcalos_path"
+    fi
+    return
+  fi
+
+  if [[ -x $command_path ]]; then
+    if ln -s "$command_path" "$arcalos_path" 2>/dev/null; then
+      return
+    fi
+  fi
+
+  printf '%s\n' '#!/bin/bash' "exec bash \"$command_path\" \"\$@\"" > "$arcalos_path"
+  chmod +x "$arcalos_path"
+}
+
 if [[ -d $ARCALOS_PATH/bin ]]; then
   for command_path in "$ARCALOS_PATH"/bin/omarchy-*; do
     [[ -e $command_path ]] || continue
-    command_name="${command_path##*/}"
-    arcalos_name="arcalos-${command_name#omarchy-}"
-    arcalos_path="$ARCALOS_PATH/bin/$arcalos_name"
-
-    if [[ ! -e $arcalos_path ]]; then
-      ln -s "$command_path" "$arcalos_path"
-    fi
+    ensure_arcalos_shim "$command_path"
   done
 fi
 
